c4da14e927141965e8098f0061ec48630284f615
nichols20 <johnnytes10@gmail.com>
2021-09-06 11:41:55 -0400

    Created genres Path

--- index.js
+++ index.js
@@ -4,13 +4,21 @@
 const express = require("express");
 //assign the created application to the app object
 const app = express();
+
 //Create a port object that will equal environemntal variabled if undefined set to 3000
 const port = process.env.PORT || 3000;
-//.get function for root path
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+
 //wrote listen function to create server
 app.listen(port, () => {
   console.log(`listening in port ${port}`);
+});
+
+//.get function for root path
+app.get("/vidly.com", (req, res) => {
+  res.send("hello world");
+});
+
+//Establishing the genres url path
+app.get("/vidly.com/api/genres", (req, res) => {
+  res.send(`genres`);
 });
