1504e2ec00f6eb739efd0570fe02484de797c78c
nichols20 <johnnytes10@gmail.com>
2021-09-06 17:05:30 -0400

    created post method for genres

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const express = require("express");
 //assign the created application to the app object
 const app = express();
 
+//Allows Json type text to be passed through the middleware
+app.use(express.json());
+
 //Create a port object that will equal environemntal variabled if undefined set to 3000
 const port = process.env.PORT || 3000;
+
+const genres = [
+  { id: 1, genre: "Action" },
+  { id: 2, genre: "Thriller" },
+  { id: 3, genre: "Comedy" },
+];
 
 //wrote listen function to create server
 app.listen(port, () => {
@@ -20,5 +29,21 @@ app.get("/vidly.com", (req, res) => {
 
 //Establishing the genres url path
 app.get("/vidly.com/api/genres", (req, res) => {
-  res.send(`genres`);
+  res.send(genres);
+});
+
+app.post("/vidly.com/api/genres", (req, res) => {
+  //Look to see if post request genre already exists
+  const duplicateGenre = genres.find(
+    (g) => g.genre.toLowerCase() === req.body.genre.toLowerCase()
+  );
+  //If the genre exists send error message set bad request status
+  if (duplicateGenre) return res.status(400).send(`genre already exists`);
+
+  //If no duplicate genre exists push requested genre to genres object
+  const genre = { id: genres.length + 1, genre: req.body.genre };
+  genres.push(genre);
+
+  //return new genres object to user
+  res.send(genres);
 });
