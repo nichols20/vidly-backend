2562eb25c5b3852fd60d1fad04c5901db1e3d7c1
nichols20 <johnnytes10@gmail.com>
2021-10-21 21:28:45 -0400

    extracted customer model & added post method

--- routes/customers.js
+++ routes/customers.js
@@ -1,11 +1,28 @@
-const express = requirw("express");
-const router = express.router();
-const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+const { Customer, validate } = require("../models/customer");
 
-const customerSchema = new mongoose.Schema({
-  isGold: Boolean,
-  name: String,
-  phone: String,
+router.get("", async (req, res) => {
+  res.send("this is the customers route");
+});
+
+router.post("", async (req, res) => {
+  const { error } = await validate(req.body);
+  if (error) {
+    res.send(error.details[0].message);
+    return;
+  }
+
+  let customer = new Customer({
+    isGold: req.body.isGold,
+    name: req.body.name,
+    phone: req.body.phone,
+  });
+
+  customer.save().then((result) => {
+    console.log(`${result} Customer information has been saved `);
+    res.send("Information has been saved");
+  });
 });
 
 module.exports = router;
