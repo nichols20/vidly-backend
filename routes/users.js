70922b9e9313ce24f72b3782ee5af9f96b3b0c8c
nichols20 <johnnytes10@gmail.com>
2021-12-16 16:39:37 -0500

    added post method and validation for user route

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,10 @@
 const { User, validate } = require("../models/users");
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
+
+router.post("", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.send(error.details[0].message).status(400);
+});
+
+module.exports = router;
