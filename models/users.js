d7938fe030ad8e10081dd055aaa90cfbb06f2b20
nichols20 <johnnytes10@gmail.com>
2021-12-16 16:11:42 -0500

    Created User Collection Model & Schema

--- models/users.js
+++ models/users.js
@@ -1,2 +1,28 @@
 const mongoose = require("mongoose");
 const joi = require("joi");
+
+const User = mongoose.model(
+  "users",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 35,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 8,
+      max: 100,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 100,
+    },
+  })
+);
