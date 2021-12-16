6fd7ac37bffedb99eb08d1280884ba90a38907d8
nichols20 <johnnytes10@gmail.com>
2021-12-16 16:42:25 -0500

    added try catch block for user route

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,21 @@ const router = express.Router();
 router.post("", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.send(error.details[0].message).status(400);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.name,
+    password: req.body.password,
+  });
+
+  try {
+    const result = await user.save();
+    console.log(result);
+    res.send(result);
+  } catch (ex) {
+    console.log(ex);
+    res.send(ex);
+  }
 });
 
 module.exports = router;
