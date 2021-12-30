abbb2755b0564eeb7aa7116018941b06bbc846bc
nichols20 <johnnytes10@gmail.com>
2021-12-30 18:42:14 -0500

    added unhandled rejection event

--- index.js
+++ index.js
@@ -25,8 +25,14 @@ const uncaughtError = require("./middleware/uncaughtException");
 //In the event of an uncaught Exception the following listener will capture the exception and store it in mongodb
 process.on("uncaughtException", (ex) => {
   console.log("UNCAUGHT EXCEPTION ERROR");
-
   uncaughtError(ex);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (ex) => {
+  console.log("UNHANDLED REJECTION");
+  uncaughtError(ex);
+  process.exit(1);
 });
 
 if (!config.get("jwtprivatekey")) {
