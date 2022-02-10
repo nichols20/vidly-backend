05191bf43d4aa24b4be3a7fb80ad2d6de4a7fe65
nichols20 <johnnytes10@gmail.com>
2022-02-10 12:14:09 -0500

    populate genres for get test

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 //When Writting integration tests I should initialize a server before each
 //test then close the server after each test.
 const request = require("supertest");
+const { Genre, validate } = require("../../models/genres");
 let server;
 
 describe("/api/genres", () => {
@@ -12,15 +13,26 @@ describe("/api/genres", () => {
     server = require("../../index");
   });
 
-  afterEach(() => {
+  afterEach(async () => {
     //close server
     server.close();
+    //cleanup genre collection
+    await Genre.remove({});
   });
 
   describe("GET", () => {
     it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
+
       const response = await request(server).get("/api/genres");
+
       expect(response.status).toBe(200);
+      expect(response.body.length).toBe(2);
     });
   });
 });
+
+//Write tests in a clean state- as if its the only test we've created.
