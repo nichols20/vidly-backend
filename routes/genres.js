9f74f57d8006cd02b6c0e933e7591b458a888218
nichols20 <johnnytes10@gmail.com>
2022-02-12 14:31:47 -0500

    added Get /:id tests & route handler

--- routes/genres.js
+++ routes/genres.js
@@ -4,12 +4,28 @@ const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const asyncMiddleware = require("../middleware/async");
 const { Genre, validate } = require("../models/genres");
+const { isValidObjectId } = require("mongoose");
 
 //Establishing the genres url path
+
 router.get(
   "",
   asyncMiddleware(async (req, res) => {
     const genre = await Genre.find().sort("asc");
+    res.send(genre);
+  })
+);
+
+router.get(
+  "/:id",
+  asyncMiddleware(async (req, res) => {
+    const isValid = isValidObjectId(req.params.id);
+    if (!isValid) return res.status(404).send("Please input a valid object ID");
+    const genre = await Genre.findById(req.params.id);
+
+    if (!genre)
+      return res.status(404).send("The genre queried could not be found ");
+
     res.send(genre);
   })
 );
--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,7 @@
 //When Writting integration tests I should initialize a server before each
 //test then close the server after each test.
+
+const mongoose = require("mongoose");
 const request = require("supertest");
 const { Genre, validate } = require("../../models/genres");
 let server;
@@ -17,7 +19,7 @@ describe("/api/genres", () => {
     //close server
     server.close();
     //cleanup genre collection
-    await Genre.remove({});
+    await Genre.deleteMany({});
   });
 
   describe("GET", () => {
@@ -30,7 +32,27 @@ describe("/api/genres", () => {
       const response = await request(server).get("/api/genres");
 
       expect(response.status).toBe(200);
-      expect(response.body.length).toBe(2);
+      expect(response.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(response.body.some((g) => g.name === "genre2")).toBeTruthy();
+    });
+
+    it("Should return a genre for a given id", async () => {
+      const _id = new mongoose.Types.ObjectId();
+
+      await Genre.collection.insertOne({ _id: _id, name: "testGenre" });
+
+      const response = await request(server).get(`/api/genres/${_id}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject({ _id: _id, name: "testGenre" });
+    });
+
+    it("should return a 404 error for bad get request", async () => {
+      const response = await request(server).get(
+        "/api/genres/909090909090909090900000"
+      );
+
+      expect(response.status).toBe(404);
     });
   });
 });
