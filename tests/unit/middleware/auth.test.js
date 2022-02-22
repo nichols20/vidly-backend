fd246aa7b71066cd4e2c15df84b817e3c0aa3e55
nichols20 <johnnytes10@gmail.com>
2022-02-22 12:39:36 -0500

    modified auth.test to be more specific

--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -1,11 +1,14 @@
 const { User } = require("../../../models/users");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 const auth = require("../../../middleware/auth");
+const mongoose = require("mongoose");
 
 describe("auth middleware", () => {
   it("should populate req.user with the payload of a valid JWT", () => {
-    const token = new User().generateAuthToken();
+    const user = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+    const token = new User(user).generateAuthToken();
 
     //before testing a function that uses the req, res, next route handlers
     //we need to manually define these objects in the test suite since express isn't being used
@@ -21,6 +24,10 @@ describe("auth middleware", () => {
 
     auth(req, res, next);
 
-    expect(req.user).toBeDefined();
+    expect(req.user.isAdmin).toBeTruthy();
+    //when expecting req.user._id to be user._id an error "serializes to same string" occured this is because
+    //even though we defined _id as an objectId the decoded payload converts the ID to be a string so to solve this error
+    //I chained the toHexString() method after generating an objectID so both properties can be compared
+    expect(req.user._id).toBe(user._id);
   });
 });
